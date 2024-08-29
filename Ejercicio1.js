ce21245666bd2d138845e545b1daf25f6527b142
olaAlfredo <128110087+olaAlfredo@users.noreply.github.com>
2024-08-28 21:20:36 -0700

    Add files via upload

---
+++ Ejercicio1.js
@@ -0,0 +1,4 @@
+// 1. Conteo de caracteres: Dada una cadena de texto y luego muestra el número de caracteres que contiene esa cadena.
+const text1 = "Hola";
+const conteo = text1.length;
+console.log("1. La cadena '"+text1+"' tiene "+conteo+" caracteres");
